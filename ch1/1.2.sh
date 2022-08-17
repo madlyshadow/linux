3916fcb4fa80dcb62bcf99345215b2295c424786
Alexey Siryk <madlyshadow@gmail.com>
2022-08-17 18:28:23 +0300

    some new scripts

---
+++ ch1/1.2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create a Bash script named script1.sh. Add the shebang.
+# Define a variable called country that holds the country you are living in.
+# Print the message: You live in YOUR_COUNTRY.
+# Run the script in different ways.
+
+country="Belarus"
+echo "You live in $country"
