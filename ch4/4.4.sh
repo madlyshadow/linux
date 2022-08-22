c44fb8c509690217cdb04c2dcb4c1add48a2236c
Alexry Siryk <madlyshadow@gmail.com>
2022-08-22 18:30:36 +0300

    ch 4.4

--- ch4/4.4.sh
+++ ch4/4.4.sh
@@ -6,26 +6,27 @@
 # Display disk usage
 # Change the Menu Prompt to the string Choose:
 # Implement each entry in the menu using the if...elif..else statements.
-#Change the solution from the previous challenge so that you use case instead of if...elif..else to implement the menu entries.
 
 PS3="Choose:"				
 select ACTION in "Display Date and Time" "List users logged in" "Display disk usage" "Quit"
 do
-	case $REPLY in
-		1)
-			date
-			;;	
-		2)
-			id 
-			;;
-		3)
-			df
-			;;
-		4)
-			echo "Quitting..."
-			sleep 1
-			exit
-	esac
+	if [[ $REPLY -eq 1 ]]
+	then 
+		date
+	elif [[ $REPLY -eq 2 ]]
+	then 
+		id
+	elif [[ $REPLY -eq 3 ]]
+	then
+		df
+	elif [[ $REPLY -eq 4 ]]
+	then
+		echo "Quitting ..."
+		sleep 1
+		exit
+	else
+		echo "Unknown selection."
+	fi
 done 
 	
 	
